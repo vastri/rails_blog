e18ee033e770e44ff0f2fe2f33e7732d777c1b51
Vastri <vastri@vastri.com>
2015-07-12 16:33:42 -0700

    Add disqus config

--- config/initializers/blog_config.rb
+++ config/initializers/blog_config.rb
@@ -6,4 +6,6 @@ AUTHOR = BLOG_CONFIG['author']
 TIME_FORMAT = BLOG_CONFIG['time_format']
 ARTICLE_FORMAT = BLOG_CONFIG['article_format']
 
+DISQUS_SHORTNAME = BLOG_CONFIG[Rails.env]['disqus_shortname']
+
 SYMBOLS = BLOG_CONFIG['symbols']
