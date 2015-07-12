c6e11ca77f098c30c7097afe125202e001273262
Vastri <vastri@vastri.com>
2015-07-11 20:59:12 -0700

    Basic layouts done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
       name = File.basename(file, ARTICLE_FORMAT)
       SYMBOLS.each { |key, symbol| name.sub!(key, "_#{symbol}_") }
       title = name.titleize
-      time = File.birthtime(file)
+      time = File.stat(file).birthtime
       path = "articles/#{CONTENTS_DIR}/#{File.basename(file)[1..-1]}"
       @articles << Article.new(title, time, path)
     end
